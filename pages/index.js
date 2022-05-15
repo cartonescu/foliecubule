a39eafb99815b3e81c1e50a2e85c2b5ad0e3b592
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-15 12:17:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,28 +16,29 @@ export default function Home() {
           Folia cu bule este un produs durabil, usor si maleabil, al carui principal beneficiu 
     este de a amortiza impactul asupra produselor impachetate. Folia cu bule este, practic, cel 
     mai usor produs de ambalare din cele existente pe piata. Folia cu bule este prietenoasa cu 
-    mediul in care traim doarece este reutilizabila. In practica, daca folia isi pastreaza 
+    mediul in care traim doarece este reutilizabila. <br />
+   <strong> In practica, daca folia isi pastreaza </strong>
     integritatea bulelor dupa ce este despachetata, ea poate fi refolosita in acelasi scop. 
     Folia cu bule a capatat notorietate in randul firmelor care au departamente logistice, deoarece 
     este un produs foarte usor de utilizat, avand o greutate mica. Folia cu bule este, practic, 
     cel mai usor produs de ambalare din cele existente pe piata. 
     Sursa text <a href="https://www.foliebule.ro">https://www.foliebule.ro</a>
-    
+    <br />
     Cum se foloseste folia cu bule si de ce este atat de bine cotata in lumea ambalajelor? Mai 
     jos vom explica pas cu pas despre cum se folosește folia cu bule pentru ambalarea diferitelor produse.
-
+  <br />
     In primul rand trebuie sa aveti o suprafata de lucru plana si curata.
     Găsiți o suprafață cat mai curată și plată pentru a vă împacheta bunurile pentru expediere. Asigurați-vă 
     că aveți spațiu suficient pentru marfa pe care doriți să o expediați și materialele de ambalare, deoarece 
-    o rola de folie cu bule poate fi voluminoasa.
-
+    o rola de <a href="https://www.cartonescu.ro/folii/folie-cu-bule">folie cu bule</a> poate fi voluminoasa.
+  <br />
      De asemenea, este recomandat să vă asigurați că aveți suficient spațiu pentru a lipi, a plia și a fixa 
     articolul fără a muta întotdeauna dintr-o parte in alta accesoriile suplimentare sau chiar produsele pe 
     care doriti sa le ambalati.
-
+  <br />
      Așezați folia cu bule astfel încât partea cu bule să fie orientată în sus. Asigurați-vă că ați așezat 
     folia cu bule astfel încât partea cu bule a filmului de plastic, să fie orientată în sus.
-    
+      <br />
     În acest fel, micile perne de aer își vor putea face treaba eficient și vor proteja bunurile dumneavoastră 
     în timpul tranzitului. In cazul in care bulele de aer vor fi dispuse către exterior, ele nu vor avea efectul 
     dorit, neputând amortiza socurile mecanice care vor apărea în timpul transportului. Asa ca, acordați atenție 
