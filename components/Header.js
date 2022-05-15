f676610af9b340e20c87afd00e262027e2c32c6b
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-15 12:45:36 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,8 @@
+<!DOCTYPE html>
+<html lang="e"ro
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
@@ -6,4 +11,37 @@ export default function Header({ title }) {
 <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
 <meta name="description" content="Folie cu bule ieftina, Folie bule impachetat - Folie cu bule ieftina - Folie bule ambalare - Folie cu bule de aer!" />
 <link rel="canonical" href="https://foliecubule.netlify.app" />
-<link rel="preload" href="/_next/static/css/stil.css" as="style"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Description" lang="en" content="open source html and css template">
+<meta name="author" content="mlp design">
+<meta name="robots" content="index, follow">
+<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="slidy.css">
+</head>
+<body>
+<div id="menu">
+<nav>
+<input type="checkbox" id="show-menu" role="button">
+<label for="show-menu" class="open"><span class="fa fa-bars"></span></label>
+<label for="show-menu" class="close"><span class="fa fa-times"></span></label>
+<ul id="topnav">  
+<li><a href="#">Custom</a></li>
+<li><a href="#">Premium</a></li>
+<li><a href="#">Gratis</a></li>
+<li><a href="#">Random</a></li>
+<li><a href="#">Search</a></li>
+<li>
+	<ul>
+	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	</ul>
+</li>
+</ul>
+</nav>
+</div>
+<div id="container">
+<div id="pageheader">
+<h1>Folie cu bule</h1>
+<p>Folie cu bule ieftina, Folie bule impachetat - Folie cu bule ieftina - Folie bule ambalare - Folie cu bule de aer!</p>
+</div>
