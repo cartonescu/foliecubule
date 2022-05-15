11ee4dd4a8b0643164d08657dcfffca3260fc235
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-15 12:30:17 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,9 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="https://gmpg.org/xfn/11">
+<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+<meta name="description" content="Folie cu bule ieftina, Folie bule impachetat - Folie cu bule ieftina - Folie bule ambalare - Folie cu bule de aer!" />
+<link rel="canonical" href="https://foliecubule.netlify.app" />
+<link rel="preload" href="/_next/static/css/stil.css" as="style"/>
