3e0726a824947dbe608df62824e12b2bbe3db1f6
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-15 12:25:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,12 +38,35 @@ export default function Home() {
   <br />
      Așezați folia cu bule astfel încât partea cu bule să fie orientată în sus. Asigurați-vă că ați așezat 
     folia cu bule astfel încât partea cu bule a filmului de plastic, să fie orientată în sus.
-      <br />
+      <br /> 
     În acest fel, micile perne de aer își vor putea face treaba eficient și vor proteja bunurile dumneavoastră 
     în timpul tranzitului. In cazul in care bulele de aer vor fi dispuse către exterior, ele nu vor avea efectul 
     dorit, neputând amortiza socurile mecanice care vor apărea în timpul transportului. Asa ca, acordați atenție 
     modului în care dispuneți folia cu bule pe produs, pentru a obține scopul propus-securizarea mărfurilor dvs.
     Mai multe <a href="https://www.foliebule.ro/folie-cu-bule">vezi aici</a>
+     <br /> <br />
+    Pe langa folia cu bule, un lat material de mare ajutor pentru ambalarea produselor este folia stretch.
+    Folia stretch pentru uz manual este o folie fabricata din polietilena extrem de subtire si flexibila, avand
+    o folosinta larga atat in mediul industrial, cat si in cel domestic. <br />
+   Desi nu are o vechime foarte mare, folia stretch a castigat rapid teren, fiind mult mai usor de utilizat decat 
+    alte ambalaje destinate protectiei obiectelor. Folia stretch manuala este o folie extensibila, utilizata cu succes 
+    in domeniul industrial si alimentar, pentru impachetarea marfurilor pe palet, dar si pentru protectia bunurilor de 
+    uz general, avand costuri logistice mult mai mici fata de metodele complementare folosite.
+ <br /> <br />
+Produsul este recomandat pentru protectia bunurilor in timpul livrarii sau stocarii, fiind o solutie ideala in conditii 
+    de umiditate, actiune mecanica sau pentru protectia impotriva particulelor de praf. Un avantaj al foliei este flexibilitatea 
+    acesteia, ceea ce o face simplu de utilizat, reducand astfel costurile din departamentele logistice, in care personalul 
+    alocat pregatirii marfurilor pentru expediere, consuma mult mai putin timp si resurse. Fiind un produs simplu, acesta 
+    poate fi utilizat chiar si de catre personal fara experienta.
+ <br /> <br />
+    <a href="https://www.foliebule.ro/folie-stretch">Folia stretch</a> manuala utilizata in mediul industrial, faciliteaza
+    fixarea si protejarea marfurilor pe paleti, asigurand buna 
+    desfasurare a procesului de transport pana la destinatar, paletii capatand un plus de stabilitate. Extrem de rezistenta 
+    la conditiile de mediu, folia stretch a ajuns un produs indispensabil pentru orice business, putand fi folosita si pentru 
+    protectia in timpul depozitarii, deoarece asigura o bariera pentru umezeala, praf, miros si gaze. Fiind o folie transparenta,
+    produsele depozitate au vizibilitate, putand fi identificate usor. 
+    <a href="https://www.cartonescu.ro/folii/folie-stretch">Vezi pe www.cartonescu.ro oferta pentru folie strech</a>
+    
         </p>
       </main>
 
